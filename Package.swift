377722f8a2cb386740b5438d404f9e872bb17f78
nglaviano <nglaviano@users.noreply.github.com>
2025-05-29 01:23:38 +0000

    feat(release): update for v1.0.2 binary distribution
      - Add/update binaryTarget with release asset URL
      - Update checksum: 85b8ef3d443f976bafed99d0d715d66f0c86b3d5dafa6e4437f5ced030208874
      - Enable binary distribution for faster builds
    
      Released by: nglaviano

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Clair",
-            url: "https://github.com/getclair/clair-ios-sdk/releases/download/v1.0.1/clair-ios-sdk.zip",
-            checksum: "a0410d7db50fc1b1a45161fa7d86f09c8165d7437c466ea1412ebaa814161001"
+            url: "https://github.com/getclair/clair-ios-sdk/releases/download/v1.0.2/clair-ios-sdk.zip",
+            checksum: "85b8ef3d443f976bafed99d0d715d66f0c86b3d5dafa6e4437f5ced030208874"
         ),
     ]
 )
