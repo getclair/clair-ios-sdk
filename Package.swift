269889dbcec737c87879abd3b928e9bd3d054d03
nglaviano <nglaviano@users.noreply.github.com>
2025-05-29 12:56:13 +0000

    feat(release): update for v1.0.10 binary distribution
      - Add/update binaryTarget with release asset URL
      - Update checksum: a2c49a6d17064ceaa82c0ce732133966af192b8506bdfa6fb8e9dfbf42b64700
      - Enable binary distribution for faster builds
    
      Released by: nglaviano

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Clair",
-            url: "https://github.com/getclair/clair-ios-sdk/releases/download/v1.0.9/clair-ios-sdk.xcframework.zip",
-            checksum: "8ad858f80387eedd8ae14f9ac6f4bf9df0d016c152f50eb6048c4b0467aa19ab"
+            url: "https://github.com/getclair/clair-ios-sdk/releases/download/v1.0.10/clair-ios-sdk.xcframework.zip",
+            checksum: "a2c49a6d17064ceaa82c0ce732133966af192b8506bdfa6fb8e9dfbf42b64700"
         ),
     ]
 )
